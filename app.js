fa9e2b0f09e234401ec23105fd0f60deba3b5aa5
evglukhov <jgluhov@gmail.com>
2017-11-10 18:02:25 +0300

    add third approach

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ render2(data);
 render2([13, 17, 19, 3]);
 
 render3(data);
+render3([13, 17, 19, 3]);
+render3([13, 17, 19, 3]);
--- approach-3.js
+++ approach-3.js
@@ -1,29 +1,11 @@
 ;(function () {
-    const fragment = document.createDocumentFragment();
-    const container = d3.select(fragment);
+    const fragment = document.createDocumentFragment(),
+        container = d3.select(fragment),
+        context = contexts[2];
 
-    window.render3 = function(data) {
-        const update = container
-            .selectAll('.rect')
-            .data(data, (d) => d);
+    clear(context);
 
-        update
-            .enter()
-            .append('rect')
-            .attr('class', 'rect')
-            .attr('x', xScale)
-            .attr('y', d3.randomUniform(0, 150))
-            .attr('size', 10)
-            .attr('fillStyle', 'red');
-
-        update
-            .exit()
-            .attr('size', 15)
-            .attr('fillStyle', 'green');
-
-        const context = contexts[2];
-        clear(context);
-
+    function draw() {
         const elements = container.selectAll('.rect');
 
         elements.each(function () {
@@ -41,6 +23,37 @@
             context.fill();
 
             context.closePath();
-        })
+        });
+    }
+
+    window.render3 = function(data) {
+        const update = container
+            .selectAll('.rect')
+            .data(data, (d) => d);
+
+        update
+            .transition()
+            .duration(1000)
+            .attr("size", 8)
+            .attr("size", 15)
+            .attr("fillStyle", "green");
+
+        update
+            .enter()
+            .append('rect')
+            .attr('class', 'rect')
+            .attr('x', xScale)
+            .attr('y', d3.randomUniform(0, 150))
+            .attr('size', 10)
+            .attr('fillStyle', 'red');
+
+        update
+            .exit()
+            .transition()
+            .duration(1000)
+            .attr('size', 5)
+            .attr('fillStyle', 'lightgrey');
     };
+
+    d3.timer(draw)
 })();
